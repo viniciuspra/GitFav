e6038d9cd17b6b30d0e222ff6da3abf688f10044
viniciuspra <93842439+viniciuspra@users.noreply.github.com>
2023-03-02 19:25:05 -0300

    Update favorites.js

--- js/favorites.js
+++ js/favorites.js
@@ -1,84 +1,96 @@
-import { GithubUser } from "./GithubUser.js"
+import { GithubUser } from "./GithubUser.js";
 
 export class Favorites {
   constructor(root) {
-    this.root = document.querySelector(root)
-    this.load()
+    this.root = document.querySelector(root);
+    this.load();
   }
 
   load() {
-    this.entries = JSON.parse(localStorage.getItem("@github-favorites:")) || []
+    // Carrega a lista de favoritos do LocalStorage ou cria um array vazio
+    this.entries = JSON.parse(localStorage.getItem("@github-favorites:")) || [];
   }
 
   save() {
-    localStorage.setItem("@github-favorites:", JSON.stringify(this.entries))
+    // Salva a lista de favoritos no LocalStorage
+    localStorage.setItem("@github-favorites:", JSON.stringify(this.entries));
   }
 
   async add(username) {
     try {
-      const userExists = this.entries.find((entry) => entry.login === username)
+      // Verifica se o usuário já existe na lista de favoritos
+      const userExists = this.entries.find((entry) => entry.login === username);
       if (userExists) {
-        throw new Error(`User ${username} already exists`)
-        return
+        throw new Error(`User ${username} already exists`);
       }
 
-      const user = await GithubUser.search(username)
+      // Busca o usuário no Github
+      const user = await GithubUser.search(username);
 
+      // Verifica se o usuário foi encontrado
       if (user.login === undefined || user.name === null) {
-        throw new Error(`User not found!`)
+        throw new Error(`User not found!`);
       }
 
-      this.entries = [user, ...this.entries]
-      this.update()
-      this.save()
+      // Adiciona o usuário à lista de favoritos
+      this.entries = [user, ...this.entries];
+      this.update();
+      this.save();
     } catch (err) {
-      alert(err.message)
+      // Exibe uma mensagem de erro caso ocorra algum problema
+      alert(err.message);
     }
   }
 
   delete(user) {
-    this.entries = this.entries.filter((entry) => entry.login !== user.login)
-    this.update()
-    this.save()
+    // Remove um usuário da lista de favoritos
+    this.entries = this.entries.filter((entry) => entry.login !== user.login);
+    this.update();
+    this.save();
   }
 }
 
 export class FavoritesView extends Favorites {
   constructor(root) {
-    super(root)
+    super(root);
+    
+    this.tbody = this.root.querySelector("table tbody");
 
-    this.tbody = this.root.querySelector("table tbody")
-    this.update()
-    this.onadd()
+    // Atualiza a lista de favoritos e configura o formulário de busca
+    this.update();
+    this.onadd();
   }
 
   onadd() {
-    const addButton = this.root.querySelector(".search button")
-    const input = this.root.querySelector(".search input")
+    // Obtém os elementos do formulário de busca
+    const addButton = this.root.querySelector(".search button");
+    const input = this.root.querySelector(".search input");
 
+    // Configura a ação do botão e da tecla Enter
     const addFavorite = () => {
-      const { value } = input
-      this.add(value)
-      input.value = ""
-      input.focus()
-    }
+      const { value } = input;
+      this.add(value);
+      input.value = "";
+      input.focus();
+    };
 
-    addButton.onclick = addFavorite
+    addButton.onclick = addFavorite;
     input.addEventListener("keydown", (event) => {
       if (event.key === "Enter") {
-        addFavorite()
+        addFavorite();
       }
-    })
+    });
   }
 
   update() {
     this.removeAllTr()
+
+    // Atualiza a tabela com os usuários favoritos
     this.entries.forEach((user) => {
       const row = this.createRow()
 
-      row.querySelector(
-        ".user img"
-      ).src = `https://github.com/${user.login}.png`
+      // Preenche as informações do usuário na linha da tabela
+      row.querySelector(".user img").src = `https://github.com/${user.login}.png`
       row.querySelector(".user img").alt = `Imagem de ${user.name}`
       row.querySelector(".user a").href = `https://github.com/${user.login}`
       row.querySelector(".user p").textContent = user.name
@@ -86,6 +98,7 @@ export class FavoritesView extends Favorites {
       row.querySelector(".repositories").textContent = user.public_repos
       row.querySelector(".followers").textContent = user.followers
 
+      // Adiciona um listener de clique para remover o usuário favorito
       row.querySelector(".remove").onclick = () => {
         const isOk = confirm(
           "Tem certeza de que deseja remover esse usuário de seus favoritos?"
@@ -95,9 +108,11 @@ export class FavoritesView extends Favorites {
         }
       }
 
+      // Adiciona a linha na tabela
       this.tbody.append(row)
     })
-    
+      
+    // Verifica se a lista de favoritos está vazia e mostra uma mensagem apropriada
     const emptyDiv = this.root.querySelector(".empty")
     if (this.entries.length === 0) {
       emptyDiv.style.display = "flex"
@@ -107,31 +122,34 @@ export class FavoritesView extends Favorites {
   }
 
   createRow() {
+    // Cria uma nova linha na tabela
     const tr = document.createElement("tr")
 
+    // Preenche a linha com o HTML necessário para mostrar as informações do usuário
     tr.innerHTML = `
-        <td class="user">
-          <img src="https://github.com/viniciuspra.png" alt="">
-          <a href="https://github.com/viniciuspra" target="_blank">
-            <p>Vinicius Pra</p>
-            <span>viniciuspra</span>
-          </a>
-        </td>
-        <td class="repositories">
-          16
-        </td>
-        <td class="followers">
-          9
-        </td>
-        <td>
-          <button class="remove">Remover</button>
-        </td>
-        `
+      <td class="user">
+        <img src="https://github.com/viniciuspra.png" alt="">
+        <a href="https://github.com/viniciuspra" target="_blank">
+          <p>Vinicius Pra</p>
+          <span>viniciuspra</span>
+        </a>
+      </td>
+      <td class="repositories">
+        16
+      </td>
+      <td class="followers">
+        9
+      </td>
+      <td>
+        <button class="remove">Remover</button>
+      </td>
+    `
 
     return tr
   }
 
   removeAllTr() {
+    // Remove todas as linhas da tabela
     this.tbody.querySelectorAll("tr").forEach((tr) => {
       tr.remove()
     })
